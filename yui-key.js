9d93a8a4fa38a2d2631174b238472adbed0badfc
Po-Chien Lin <linpc@cs.nctu.edu.tw>
2014-11-12 23:29:45 +0800

    Add switch to turn on/off detection

--- yui-key.js
+++ yui-key.js
@@ -1,5 +1,22 @@
 YUI().use('node', 'node-event-simulate', function(Y) {
-	var ck = function(e) {
+	var isSwitchOption = function (e) {
+		return ((!e.ctrlKey && e.altKey && e.shiftKey) === true && e.keyCode === 79);
+	}
+
+	var switchMode = 0;
+	var processSwitch = function (e) {
+		if (isSwitchOption(e) === true) {
+			switchMode = (switchMode + 1) % 2;
+		}
+		Y.one('#mode').set('text', switchMode);
+	}
+
+	var ck = function (e) {
+
+		processSwitch(e);
+		if (switchMode == 0) {
+			return;
+		}
 
 		var kstr = '<tr>';
 		kstr += ('<td>' + Date().substr(4, 20) + '</td>');

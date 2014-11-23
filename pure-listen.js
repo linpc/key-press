c040bf7bb9bb599ed0a451781c753ba1bbbb109c
Po-Chien Lin <linpc@cs.nctu.edu.tw>
2014-11-23 22:13:33 +0800

    Comment out document.write funtion to make listener work

--- pure-listen.js
+++ pure-listen.js
@@ -1,13 +1,13 @@
 document.onkeydown = function (e) {
 	console.log(e);
-	document.write('<br>');
-	document.write('keyCode: ' + e.keyCode + '<br>');
-	document.write('altKey: ' + e.altKey + '<br>');
-	document.write('shiftKey: ' + e.shiftKey + '<br>');
-	document.write('charCode: ' + e.charCode + '<br>');
-	document.write('ctrlKey: ' + e.ctrlKey + '<br>');
-	document.write('keyIdentifier: ' + e.keyIdentifier + '<br>');
-	document.write('metaKey: ' + e.metaKey + '<br>');
-	document.write('timeStamp: ' + e.timeStamp + '<br>');
-	document.write('type: ' + e.type + '<br>');
+//	document.write('<br>');
+//	document.write('keyCode: ' + e.keyCode + '<br>');
+//	document.write('altKey: ' + e.altKey + '<br>');
+//	document.write('shiftKey: ' + e.shiftKey + '<br>');
+//	document.write('charCode: ' + e.charCode + '<br>');
+//	document.write('ctrlKey: ' + e.ctrlKey + '<br>');
+//	document.write('keyIdentifier: ' + e.keyIdentifier + '<br>');
+//	document.write('metaKey: ' + e.metaKey + '<br>');
+//	document.write('timeStamp: ' + e.timeStamp + '<br>');
+//	document.write('type: ' + e.type + '<br>');
 };
